0da26a6b22f752bb9e764af41d0ea67caa2fcf1b
RaMich271de <stellarator@t-online.de>
2026-03-22 19:17:41 +0100

    Delete usr/local/bin directory

--- usr/local/bin/arp-scan-wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Nur IP-Zeilen ausgeben - keine Header/Footer von arp-scan
-/usr/bin/arp-scan --interface=eth0 --localnet --quiet 2>/dev/null | \
-    grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
--- usr/local/bin/update-weblink.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-VM_IP=$(ip addr show eth0 | grep 'inet ' | head -1 | awk '{print $2}' | cut -d/ -f1)
-printf '[InternetShortcut]\nURL=http://%s/\n' "$VM_IP" > /var/lib/proxy/weblink/Webinterface.url
-echo "Webinterface.url: http://$VM_IP/"
